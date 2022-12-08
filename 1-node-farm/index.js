8de4c7c6d9089d7ac6a2a669d4f384dce98796f9
KyoldFusion <michaeltyler96@icloud.com>
2022-12-08 10:56:51 -0600

    UpdatedCOde

---
+++ 1-node-farm/index.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const http = require('http');
+
+// Files
+//////////////////////////
+
+// Blocking, Synchronous way
+// const textIn = fs.readFileSync('./starter/txt/input.txt', 'utf-8');
+// console.log(textIn);
+// const textOut = `This is what we know baout the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./starter/txt/output.txt', textOut);
+// console.log('File Written!');
+
+
+
+// Non-Blocking, asynchronous way
+// fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data1) => {
+//     if (err) return console.log('error')
+//     fs.readFile(`./starter/txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2);
+//         fs.readFile('./starter/txt/append.txt', 'utf-8', (err, data3) => {
+//             console.log(data3);
+
+//             fs.writeFile('./starter/txt/final.txt', `${data2}\n ${data3}`, 'utf-8', err => {
+//                 console.log('File has been written.');
+//             })
+//         })
+//     })
+// })
+// console.log('Will read file');
+
+/////////////////////////////////////
+// SERVER
+const server = http.createServer((req, res) => {
+    res.end('Hello from the server!');
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000');
+});

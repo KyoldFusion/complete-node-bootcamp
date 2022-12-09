61a452b99263bdc1436246b9c0e64b22ce0fd5e3
KyoldFusion <michaeltyler96@icloud.com>
2022-12-09 10:55:16 -0600

    fixed_productname issue

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -19,20 +19,18 @@ const replaceTemplate = (temp, product) => {
 }
 
 
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8', (err, data) =>{
-});
-const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8', (err, data) =>{
-});
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8', (err, data) =>{
-});
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) =>{
-});
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 
 const server = http.createServer((req, res) => {
-    const {query, pathname }= url.parse(req.url, true);
+    const { query, pathname } = url.parse(req.url, true);
+    console.log(query);
+
 
     //Overview Page
     if(pathname === '/' || pathname === '/overview'){
